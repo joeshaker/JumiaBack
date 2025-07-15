60003a0fd92a03474c7e0f41f700d2f2cd08b6c7
Ali Abooof <aliabooof@outlook.com>
2025-07-15 17:25:47 +0300

    products Dtos

--- Jumia-Api.Application/Interfaces/IProductService.cs
+++ Jumia-Api.Application/Interfaces/IProductService.cs
@@ -10,6 +10,11 @@ namespace Jumia_Api.Application.Interfaces
 {
     public interface IProductService
     {
+        public Task<IEnumerable<Product>> GetAllProductsAsync();
+        public Task<IEnumerable<Product>> GetAvailableProductsAsync();
+        public Task<ProductDto> GetProductByIdAsync(int productId);
+        public Task UpdateProduct(UpdateProductDto product);
+        public Task CreateProduct(AddProductDto product);
         public Task<IEnumerable<Product>> GetProductsByCategoriesAsync(ProductFilterRequestDto productFilterRequestDto);
 
     }
