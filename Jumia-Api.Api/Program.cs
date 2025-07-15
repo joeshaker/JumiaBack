ce22c8dec79a7c7aca6a0a2509509709267d8055
Abdelkader15 <https://github.com/Abdelkader15>
2025-07-15 13:47:43 +0300

    Enable Swagger

--- Jumia-Api.Api/Program.cs
+++ Jumia-Api.Api/Program.cs
@@ -1,5 +1,6 @@
 
 using Jumia_Api.Api.DependencyInjection.Infrastructure;
+using Microsoft.OpenApi.Models;
 
 namespace Jumia_Api.Api
 {
@@ -14,15 +15,28 @@ namespace Jumia_Api.Api
             builder.Services.AddControllers();
             builder.Services.AddInfrastructure(builder.Configuration);
 
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            // Swagger/OpenAPI configuration
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Jumia API",
+                    Version = "v1",
+                    Description = "API for Jumia Application",
+                });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jumia API v1");
+                });
             }
 
             app.UseHttpsRedirection();
