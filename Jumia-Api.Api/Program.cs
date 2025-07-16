a7bf69e63a0667981d797fdf3f805669b870e3b6
mariamkashkoush26@gmail.com <mariamkashkoush26@gmail.com>
2025-07-16 11:11:49 +0300

    auth

--- Jumia-Api.Api/Program.cs
+++ Jumia-Api.Api/Program.cs
@@ -1,5 +1,16 @@
 
 using Jumia_Api.Api.DependencyInjection.Infrastructure;
+using Jumia_Api.Application.Interfaces;
+using Jumia_Api.Application.Services;
+using Jumia_Api.Domain.Models;
+using Jumia_Api.Infrastructure.Presistence.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace Jumia_Api.Api
 {
@@ -10,23 +21,79 @@ namespace Jumia_Api.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
 
             builder.Services.AddControllers();
-            builder.Services.AddInfrastructure(builder.Configuration);
+            //builder.Services.AddInfrastructure(builder.Configuration);
+
+            builder.Services.AddDbContext<JumiaDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("JumiaContextConnection")));
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<JumiaDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.AddScoped<IAuthService, AuthService>();
+
+            var jwtConfig = builder.Configuration.GetSection("Jwt");
+            //builder.Services.Configure<JwtOptions>(jwtConfig);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtConfig["Issuer"],
+                    ValidAudience = jwtConfig["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtConfig["Key"]!)),
+                    //ClockSkew = TimeSpan.Zero
+
+                };
+                //options.Events = new JwtBearerEvents
+                //{
+                //    OnMessageReceived = context =>
+                //    {
+                //        var token = context.Request.Cookies["jwt"];
+                //        if (!string.IsNullOrEmpty(token))
+                //        {
+                //            context.Token = token;
+                //        }
+                //        return Task.CompletedTask;
+                //    }
+                //};
+
+            });
+
 
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            //builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
+            //Enable Swagger middleware
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                //app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
