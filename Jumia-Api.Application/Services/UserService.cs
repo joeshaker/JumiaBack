d7fbf545e4aa57d12fb6868c0cff90139264f635
mariamkashkoush26@gmail.com <mariamkashkoush26@gmail.com>
2025-07-17 01:50:48 +0300

    Authentication

---
+++ Jumia-Api.Application/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using Jumia_Api.Application.Interfaces;
+using Jumia_Api.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumia_Api.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserService(UserManager<AppUser> userManager)
+        {
+             _userManager = userManager;
+        }
+        public async Task<bool> CheckPasswordAsync(AppUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+
+        public async Task<IdentityResult> CreateUserAsync(string email, string password)
+        {
+            var user = new AppUser
+            {
+                UserName = email,
+                Email = email,
+                // Default values for testing
+                FirstName = "Test",
+                LastName = "User",
+                DateOfBirth = new DateTime(2000, 1, 1),
+                Address = "123 Main Street",
+                Gender = "Female" // or "Male", "Other", etc.
+            };
+            return await _userManager.CreateAsync(user,password);
+
+        }
+
+        public Task<AppUser> FindByEmailAsync(string email)
+        {
+            return _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<bool> UserExistsAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email) != null;
+        }
+    }
+}
