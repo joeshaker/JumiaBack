3965b0830a024d5e1b7f5c2608853d28971f9174
Ali Abooof <aliabooof@outlook.com>
2025-07-22 17:34:41 +0300

    semantic search and fuzzy search product ai

--- Jumia-Api.Api/DependencyInjection/Infrastructure/InfrastructureServicesRegistraion.cs
+++ Jumia-Api.Api/DependencyInjection/Infrastructure/InfrastructureServicesRegistraion.cs
@@ -1,7 +1,10 @@
-﻿using Jumia_Api.Domain.Models;
+﻿using Jumia_Api.Application.Interfaces;
+using Jumia_Api.Application.Services;
+using Jumia_Api.Domain.Models;
 using Jumia_Api.Infrastructure.Presistence.Context;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Qdrant.Client;
 
 namespace Jumia_Api.Api.DependencyInjection.Infrastructure
 {
@@ -15,6 +18,10 @@ namespace Jumia_Api.Api.DependencyInjection.Infrastructure
             services.AddIdentity<AppUser, IdentityRole>()
                 .AddEntityFrameworkStores<JumiaDbContext>()
                 .AddDefaultTokenProviders();
+
+
+            services.AddScoped<IProductAiService, ProductAiService > ();
+            services.AddSingleton(new QdrantClient(host: "localhost", port: 6334, https: false));
 
 
             return services;
